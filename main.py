62c3a90af3a66bda767c66d2319c28880a8f444e
AbouTheGreat <72754592+AbouTheGreat@users.noreply.github.com>
2021-02-01 00:53:35 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
 while True:
-    print("The current pitch is at "+ input.rotation(Rotation.PITCH))
+    light.set_all(color.rgb(0, 255, 0))
+    print("The current acceleration(going up) is "+input.acceleration(Dimension.Z))
+    if input.acceleration(Dimension.Z) > 1400:
+        print("This elevator is moving way too fast")
+        light.set_all(color.rgb(255, 0, 0))
+        music.siren.play_until_done()
+
+    elif -7 > input.rotation(Rotation.ROLL) or input.rotation(Rotation.ROLL) > 7 :
+        print("error, elevator rolling too much") 
+        light.set_all(color.rgb(255, 255, 0))
+        music.siren.play_until_done()
+    elif -7 > input.rotation(Rotation.PITCH) or input.rotation(Rotation.PITCH) > 7 :
+        print("error, elevator rolling too much")
+        light.set_all(color.rgb(255, 255, 55))
+        music.siren.play_until_done()
+        
+    
+
--- main.ts
+++ main.ts
@@ -1,3 +1,18 @@
 while (true) {
-    console.log("The current pitch is at " + input.rotation(Rotation.Pitch))
+    light.setAll(color.rgb(0, 255, 0))
+    console.log("The current acceleration(going up) is " + input.acceleration(Dimension.Z))
+    if (input.acceleration(Dimension.Z) > 1400) {
+        console.log("This elevator is moving way too fast")
+        light.setAll(color.rgb(255, 0, 0))
+        music.siren.playUntilDone()
+    } else if (-7 > input.rotation(Rotation.Roll) || input.rotation(Rotation.Roll) > 7) {
+        console.log("error, elevator rolling too much")
+        light.setAll(color.rgb(255, 255, 0))
+        music.siren.playUntilDone()
+    } else if (-7 > input.rotation(Rotation.Pitch) || input.rotation(Rotation.Pitch) > 7) {
+        console.log("error, elevator rolling too much")
+        light.setAll(color.rgb(255, 255, 55))
+        music.siren.playUntilDone()
+    }
+    
 }
